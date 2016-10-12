ac43efe627d50955301a7179f5a92b08dc6b4e1d
Bruno Dias <bruno.r.dias@gmail.com>
2016-10-12 20:20:43 -0300

    Styling and other changes

--- build/blotter.js
+++ build/blotter.js
@@ -46,17 +46,30 @@
 
 	const inkjs = __webpack_require__(1);
 
-	const storySource = document.querySelector('script[type="application/json"]');
+	const storySource = document.querySelector('script[id="storyscript"][type="application/json"]');
 
-	const story = new inkjs.Story(storySource.innerHTML);
+	if (!storySource) {
+	  throw new Error('Inline story data missing from page.');
+	}
 
-	window.story = story; // For debug purposes
-
-	/* Set up the stage */
+	/* Find the elements where we'll be putting content */
 
 	const stageDiv = document.querySelector('#story-stage');
 	const choiceDiv = document.querySelector('#choices');
 
+	if (!stageDiv || !choiceDiv) {
+	  throw new Error('Unable to select content and choice container elements.');
+	}
+
+	/* At least on Windows, inklecate seems to put some whitespace character at the
+	   head of generated json files that causes JSON.parse to choke, at least on
+	   webkit. This is fixed by using trim() on the string first. */
+	const storyData = JSON.parse(storySource.innerText.trim());
+	console.log('Story data parsed...');
+	const story = new inkjs.Story(storyData);
+	window.story = story;
+
+	/* Page scrolling code */
 	const scroll = {
 	  scrolling: false,
 
@@ -79,16 +92,63 @@
 	  }
 	};
 
+	function emphasis (line) {
+	  const grodyRegex = /(\s|^)(_{1,2})(\w[^_]*\S)(\2)(\W)/;
+	  if (!line.match(grodyRegex)) return line; // No italics here
+	  const alteredLine = line.replace(grodyRegex, (match, p1, p2, p3, p4, p5) => {
+	    if (p2 === '_') return `${p1}<em>${p3}</em>${p5}`;
+	    return `${p1}<strong>${p3}</strong>${p5}`
+	  });
+	  return emphasis(alteredLine);
+	}
+
+	function quoter (line) {
+	  const grodyRegex = /(^|\s)(['"])(.+?)(\2)(\W|$)/;
+	  if (!line.match(grodyRegex)) return line;
+	  const alteredLine = line.replace(grodyRegex, (match, p1, p2, p3, p4, p5) => {
+	    if (p2 === '"') return `${p1}&ldquo;${p3}&rdquo;${p5}`;
+	    return `${p1}&lsquo;${p3}&rsquo;${p5}`
+	  });
+	  return quoter(alteredLine);
+	}
+
+	function dasher (line) {
+	  return line
+	    .replace(/ - /g, ' &ndash; ')
+	    .replace(/ -- /g, '&mdash;');
+	}
+
+	function format (line) {
+	  return emphasis(quoter(dasher(line)));
+	}
+
+	/* Create an element (usually p, but also h1-h6) and format it. */
+	function createLineElement (line) {
+	  let elemType = 'p', content = line;
+	  const matches = line.match(/^(h\d) (.+)/);
+	  if (matches) {
+	    // Line starts with the name of a title element so let's use that
+	    elemType = matches[1];
+	    content = matches[2];
+	  }
+	  const elem = document.createElement(elemType);
+	  elem.innerHTML = format(content);
+	  return elem;
+	}
+
+	/* Insert a line of content into the output */
+	/* Returns a Promise that resolves when the animation on the inserted element
+	   ends. */
 	function insertLine () {
 	  const line = story.Continue();
-	  const p = document.createElement('p');
-	  p.innerText = line;
+	  const elem = createLineElement(line);
 	  return new Promise(resolve => {
-	    p.addEventListener('animationend', resolve);
-	    stageDiv.appendChild(p);
+	    elem.addEventListener('animationend', resolve);
+	    stageDiv.appendChild(elem);
 	  });
 	}
 
+	/* The main game loop; progress the story by an additional step. */
 	function progressGame () {
 	  scroll.start();
 	  insertLine().then(() => {
@@ -101,8 +161,12 @@
 	  })
 	}
 
+	// This flag is adhockery to prevent a race condition where the player can
+	// click on a choice before they have fully animated in.
 	let choicesAnimating = false;
 
+	// Create a listener function to attach to one of the li elements that
+	// represents a player choice.
 	function choiceListener(index) {
 	  return function (event) {
 	    event.preventDefault();
@@ -113,6 +177,7 @@
 	  }
 	}
 
+	// Attach that listener to the element.
 	function createChoiceListener(li, index) {
 	  li.clickListener = choiceListener(index);
 	  li.addEventListener('click', li.clickListener);
@@ -121,6 +186,7 @@
 	  }).bind(li);
 	}
 
+	// Clean up event listeners from choices once one of them was selected.
 	function clearChoiceListeners() {
 	  const lis = document.querySelectorAll('li');
 	  Array.from(lis).forEach(li => {
@@ -128,6 +194,7 @@
 	  })
 	}
 
+	// Create the ul element that contains a batch of choices.
 	function createChoiceUl () {
 	  const lis = story.currentChoices
 	    .map(choice => {
@@ -142,6 +209,7 @@
 	  return ul;
 	}
 
+	// Insert a choice list into the container div.
 	function placeChoices () {
 	  const ul = createChoiceUl();
 	  ul.addEventListener('animationend', () => choicesAnimating = false);
@@ -149,6 +217,7 @@
 	  choiceDiv.appendChild(ul);
 	}
 
+	// Mark used choice lists as old and remove them from the view.
 	function clearOldChoices () {
 	  const oldChoices = choiceDiv.querySelector('.choices.current');
 	  return new Promise(resolve => {
@@ -160,7 +229,14 @@
 	  })
 	}
 
-	progressGame();
+	window.blotterStart = function () {
+	  // For simplicity's sake, we want Blotter to be essentially a black box that
+	  // talks to separate scripts on the same page through some well-defined APIs
+	  // rather than a module which would be webpacked along with user-provided
+	  // scripts. This prevents Gall from having to use npm or webpack or browserify
+	  // itself.
+	  progressGame();
+	}
 
 
 /***/ },
--- test/example.ink
+++ test/example.ink
@@ -1,15 +1,20 @@
-This is a simple example game written with Ink and compiled using Inkle's own Inky IDE.
+h1 Blotter Game Example
+This is a simple example game written with Ink and compiled using __Inkle__'s own Inky IDE.
 *   [Continue]
     -> choices
 = choices
 *   [About Ink]
-    Ink is an open source scripting language for writing interactive narratives, developed originally by Inkle.
+    h2 About Ink
+    _Ink_ is an open source scripting language for writing interactive narratives, developed originally by Inkle.
     -> choices
 *   [About Inkjs]
-    Inkjs is an isomorphic javascript port of Ink, for both Node and the browser.
+    h2 About Inkjs
+    _Inkjs_ is an isomorphic javascript port of Ink, for both Node and the browser.
     -> choices
 *   [About Blotter]
-    Blotter is a frontend engine for Inkjs meant to run on the web or within hybrid platforms like Electron and Cordova. It uses inkjs as its core game engine, and is otherwise a dependencyless implementation of a front-end interface for Ink stories.
+    h2 About Blotter
+    _Blotter_ is a frontend engine for Inkjs meant to run on the web or within hybrid platforms like Electron and Cordova. It uses inkjs as its core game engine, and is otherwise a dependencyless implementation of a front-end interface for Ink stories.
+    It includes some basic formatting features, including _emphasis_, __strong__, 'smart' "quotes", and dashes - both en and -- em.
     -> choices
 *   ->  ending
 = ending
